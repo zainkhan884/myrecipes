af81f7c2e663843a8e4483c79b79956c70eb4dfd
zainkhan884 <zaink8282@gmail.com>
2020-06-08 00:05:16 +0530

    Add TDD and index listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 	root "pages#home"
 	get 'pages/home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/recipes', to: "recipes#index"
 end
---
+++ test/integration/recipes_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class RecipesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @chef = Chef.create(chefname: "zain", email: "zaink@gmail.com")
+    @recipe = Recipe.create(name: "vegetables market", description: "great vegetables market with good quality", chef:  @chef)
+    @recipe2 = Recipe.create(name: "chicken chilly", description: "great chicken taste", chef: @chef)
+  end
+
+
+ test "should get recipes index" do
+   get recipes_path
+   assert_response  :success
+ end	
+ 
+  test "should get recipes listing" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body
+    assert_match @recipe2.name, response.body
+  end
+end
