c2932d2945a5f3c18bc8accac60fd6c6eff28113
zainkhan884 <zaink8282@gmail.com>
2020-06-09 02:04:30 +0530

    first commit

--- app/models/recipe.rb
+++
@@ -1,6 +0,0 @@
-class Recipe < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true, length: {minimum: 5, maximum: 200}
-  belongs_to :chef
-  validates :chef_id, presence: true
-end
